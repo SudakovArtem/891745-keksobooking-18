3b5361fd6f0c13773d121c98ec040b7f39e73c57
SudakovArtem <tyomik1000@gmail.com>
2019-09-23 13:29:18 +0300

    выполнил задание личного проекта: больше деталей

--- js/main.js
+++ js/main.js
@@ -76,8 +76,8 @@ var getMock = function () {
       },
 
       'offer': {
-        'title': 'title text', // строка, заголовок предложения
-        'address': '600, 350', // строка, адрес предложения. Для простоты пусть пока представляет собой запись вида '{{location.x}}, {{location.y}}', например, '600, 350'
+        'title': 'Заголовок объявления', // строка, заголовок предложения
+        'address': 'Адрес предложения', // строка, адрес предложения. Для простоты пусть пока представляет собой запись вида '{{location.x}}, {{location.y}}', например, '600, 350'
         'price': getRandomInRange(COST_MIN, COST_MAX) + addZeros('', 3), // число, стоимость
         'type': getRandom(TYPE), // строка с одним из четырёх фиксированных значений: palace, flat, house или bungalo
         'rooms': getRandomInRange(NUMBER_OF_ROOMS_MIN, NUMBER_OF_ROOMS_MAX), // число, количество комнат
@@ -85,7 +85,7 @@ var getMock = function () {
         'checkin': getRandomInRange(CHECK_IN_OUT_MIN, CHECK_IN_OUT_MAX) + ':' + addZeros('', 2), // строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00,
         'checkout': getRandomInRange(CHECK_IN_OUT_MIN, CHECK_IN_OUT_MAX) + ':' + addZeros('', 2), // строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00
         'features': getRandomList(FEATURES, FEATURES_START, FEATURES.length), // массив строк случайной длины из ниже предложенных: 'wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner',
-        'description': 'description text', // строка с описанием,
+        'description': 'Описание объекта недвижимости', // строка с описанием,
         'photos': getRandomList(PHOTOS, PHOTOS_START, PHOTOS.length), // массив строк случайной длины, содержащий адреса фотографий 'http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
       },
 
@@ -110,6 +110,54 @@ var renderPin = function (elem) {
   return pinElement;
 };
 
+// ---------------------Задание 3--------------------------------------------
+
+var renderCard = function (elem) {
+  var mapCardTemplate = document.querySelector('#card').content.querySelector('.map__card');
+  var cardElement = mapCardTemplate.cloneNode(true);
+  var photos = cardElement.querySelector('.popup__photos');
+
+  var getPhotos = function (val) {
+    val.innerHTML = '';
+    for (var i = 0; i < elem.offer.photos.length; i++) {
+      val.insertAdjacentHTML('afterbegin', '<img src="' + elem.offer.photos[i] + '" class="popup__photo" width="45" height="40" alt="Фотография жилья">');
+    }
+  };
+
+  var getType = function () {
+    switch (elem.offer.type) {
+      case 'palace':
+        cardElement.querySelector('.popup__type').textContent = 'Дворец';
+        break;
+      case 'flat':
+        cardElement.querySelector('.popup__type').textContent = 'Квартира';
+        break;
+      case 'house':
+        cardElement.querySelector('.popup__type').textContent = 'Дом';
+        break;
+      case 'bungalo':
+        cardElement.querySelector('.popup__type').textContent = 'Бунгало';
+        break;
+    }
+  };
+
+  cardElement.querySelector('.popup__title').textContent = elem.offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = elem.offer.address;
+  cardElement.querySelector('.popup__text--price').textContent = elem.offer.price + '₽/ночь';
+  getType();
+  cardElement.querySelector('.popup__text--capacity ').textContent = elem.offer.rooms + ' комнаты для ' + elem.offer.guests + ' гостей';
+  cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + elem.offer.checkin + ', выезд до ' + elem.offer.checkout;
+  cardElement.querySelector('.popup__features').textContent = elem.offer.features.toString();
+  cardElement.querySelector('.popup__description').textContent = elem.offer.description;
+  cardElement.querySelector('.popup__photos > img').src = elem.offer.photos[0];
+  getPhotos(photos);
+  cardElement.querySelector('.popup__avatar').src = elem.author.avatar;
+
+  return cardElement;
+};
+
+// ---------------------Задание 3--------------------------------------------
+
 var getSimilarAdverts = function () {
   var map = document.querySelector('.map');
   map.classList.remove('map--faded');
@@ -119,6 +167,7 @@ var getSimilarAdverts = function () {
   for (var i = 0; i < offers.length; i++) {
     fragment.appendChild(renderPin(offers[i]));
   }
+  fragment.appendChild(renderCard(offers[0])); // Задание 3
   similarMapPin.appendChild(fragment);
   return offers;
 };
