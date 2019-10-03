c28787d7c00edcd3a1f9010ab451739d7b4659e5
SudakovArtem <tyomik1000@gmail.com>
2019-10-03 13:54:32 +0300

    начал второе задание5 раздела

--- js/map.js
+++ js/map.js
@@ -22,11 +22,61 @@
       mapPinMain.style.top; // расстояние до острого конца по вертикали
   };
 
-  var mapPinClickHandler = function () {
+
+
+  // --------------------------------------------------------------------------------------------------------------
+
+  var mapPinClickHandler = function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY,
+    };
+
+    var dragged = false;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY,
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY,
+      };
+
+      mapPinMain.style.top = (mapPinMain.offsetTop - shift.y) + 'px';
+      mapPinMain.style.left = (mapPinMain.offsetLeft - shift.x) + 'px';
+
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+
     window.makePageActive();
     mapPinMain.removeEventListener('mousedown', mapPinClickHandler);
     mapPinMain.removeEventListener('keydown', mapPinPressEnterHandler);
   };
+
+
+  //- ---------------------------------------------------------------------------------------------------------------
+
+
+
+
 
   var mapPinPressEnterHandler = function (evt) {
     if (evt.keyCode === ENTER_KEYCODE) {
