b30f813c1389b4fad53308839902409a7b3f662c
Артём <tyomik1000@gmail.com>
2019-10-12 20:13:32 +0300

    сделал второе задание 7 раздела

--- js/filter.js
+++ js/filter.js
@@ -1,9 +1,11 @@
 'use strict';
 
 (function () {
-  var filterForm = window.map.getMapElement().querySelector('.map__filters');
-  var housingType = filterForm.querySelector('#housing-type');
+  var LOW_PRICE = 10000;
+  var HIGH_PRICE = 50000;
+
   var fragment = window.util.getFragment();
+  var filterForm = document.querySelector('.map__filters');
 
   var addNode = function (elem) {
     for (var i = 0; i < window.page.getTakeNumber(elem); i++) {
@@ -12,17 +14,66 @@
     }
   };
 
-  housingType.addEventListener('change', function (elem) {
-    window.page.removePins();
-    if (housingType.value !== 'any') {
-      var takeNumber = 5;
-      elem = window.pins.slice().filter(function (item) {
-        return item.offer.type === housingType.value;
+  var filterByNumber = function (formData, filteredData, attr) {
+    var value = formData.get('housing-' + attr);
+
+    if (value !== 'any') {
+      filteredData = filteredData.filter(function (pin) {
+        return pin.offer[attr] === parseInt(value, 10);
       });
-      addNode(elem, takeNumber);
-    } else {
-      elem = window.pins;
-      addNode(elem);
     }
+
+    return filteredData;
+  };
+
+  var getFilterData = function () {
+    var filteredData = window.data.data;
+    var formData = new FormData(filterForm);
+    var features = [];
+
+    filterForm.querySelectorAll('.map__features input:checked').forEach(function (checkbox) {
+      features.push(checkbox.value);
+    });
+
+    if (formData.get('housing-type') !== 'any') {
+      filteredData = filteredData.filter(function (pin) {
+        return pin.offer.type === formData.get('housing-type');
+      });
+    }
+
+    if (formData.get('housing-price') !== 'any') {
+      filteredData = filteredData.filter(function (pin) {
+        var price = 'middle';
+
+        if (pin.offer.price < LOW_PRICE) {
+          price = 'low';
+        } else if (pin.offer.price > HIGH_PRICE) {
+          price = 'high';
+        }
+
+        return price === formData.get('housing-price');
+      });
+    }
+
+    filteredData = filterByNumber(formData, filteredData, 'rooms');
+
+    filteredData = filterByNumber(formData, filteredData, 'guests');
+
+    if (features.length > 0) {
+      features.forEach(function (feature) {
+        filteredData = filteredData.filter(function (pin) {
+          return pin.offer.features.includes(feature);
+        });
+      });
+    }
+
+    return filteredData;
+  };
+
+  filterForm.addEventListener('change', function () {
+    window.page.removePins();
+    var filteredData = getFilterData();
+
+    addNode(filteredData);
   });
 })();
